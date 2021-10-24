4cbe8db80fe9d9099c488dc399f7aeed2204589b
syedahalam <syedahalam@yahoo.com>
2021-10-24 01:14:55 -0400

    MVP Done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,58 +1,54 @@
 import React from 'react';
-import './Navbar.css'
+import './Navbar.css';
 // font awesome import
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 	return (
-		<nav className='navbar navbar-expand-lg navbar-light bg-dark'>
-            <div className="container">
+		<nav className='navbar navbar-expand-lg navbar-light bg-dark sticky-top'>
+			<div className='container'>
+				<Link className='navbar-brand' href='#'>
+					SHA
+				</Link>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-toggle='collapse'
+					data-target='#navbarSupportedContent'
+					aria-controls='navbarSupportedContent'
+					aria-expanded='false'
+					aria-label='Toggle navigation'>
+					<FontAwesomeIcon icon={faBars} style={{ color: 'white' }} />
+				</button>
 
-			<a className='navbar-brand' href='#' >
-				SHA
-			</a>
-			<button
-				className='navbar-toggler'
-				type='button'
-				data-toggle='collapse'
-				data-target='#navbarSupportedContent'
-				aria-controls='navbarSupportedContent'
-				aria-expanded='false'
-				aria-label='Toggle navigation'>
-				<FontAwesomeIcon  icon={faBars} style={{ color:'white'}}/>
-			</button>
-
-			<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-				<ul className='navbar-nav m-lg-auto'>
-					<li className='nav-item active'>
-						<a className='nav-link' href='#'>
-							Home 
-						</a>
-					</li>
-					<li className='nav-item'>
-						<a className='nav-link' href='#'>
-							About
-						</a>
-					</li>
-					<li className='nav-item'>
-						<a className='nav-link' href='#'>
-							Resume
-						</a>
-					</li>
-					<li className='nav-item'>
-						<a className='nav-link' href='#'>
-							Contact
-						</a>
-					</li>
-					<li className='nav-item'>
-                        <a className='nav-link' href='#'>
-							Skills
-						</a>
-					</li>
-				</ul>
+				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+					<ul className='navbar-nav m-lg-auto'>
+						<li className='nav-item active'>
+							<Link smooth={true} to='home' className='nav-link' href='#'>
+								Home
+								<span className='sr-only'>(current)</span>
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link smooth={true} to='about'  className='nav-link' href='#'>
+								About
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link smooth={true} to='projects'  className='nav-link' href='#'>
+								Projects
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link smooth={true} to='contactme' className='nav-link' href='#'>
+								Contact
+							</Link>
+						</li>
+					</ul>
+				</div>
 			</div>
-                            </div>
 		</nav>
 	);
 };
