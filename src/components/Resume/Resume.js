829c1019e74f32e727116d8a31fdbc96daaaa45b
syedahalam <syedahalam@yahoo.com>
2021-10-24 21:02:08 -0400

    Resume

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+			<div id='resume'>
+				{/* <h1>Hi from resume</h1> */}
+				<iframe src='https://docs.google.com/document/d/e/2PACX-1vQxAGt5sl2FL3O3sYLymckeCybAu41LQA2xcvsJNIoHHqRkkL5R1foQgrRymf5CmO8zzQBjhMvSiaaA/pub?embedded=true' width='100%' height='700'></iframe>
+			</div>
+		);
+}
+
+export default Resume
